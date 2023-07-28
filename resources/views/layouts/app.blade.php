580192bfe20b1b9b81e30490f4be1ca49d120774
ShirokamiRyzen <infinixgaming360@gmail.com>
2023-07-28 14:35:22 +0700

    migrate to laravel 10

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Favicons -->
+    <link href="{{ $logo }}" rel="icon">
+    <link rel="stylesheet" href="assets/css/styles.css">
+
+    <script src="assets/js/three.r134.min.js"></script>
+    <script src="assets/js/vanta.halo.min.js"></script>
+
+    <video id="bg-video" muted loop autoplay poster="assets/poster.png">
+        <source src="https://github.com/lknknm/link-in-bio/raw/main/assets/links-bg.webm" type="video/webm">
+    </video>
+    <div class="overlay"></div>
+    <title>@yield('header')</title>
+</head>
+
+@yield('content')
+
+<footer>
+    <p>Made with ❤️ by <a href="https://github.com/ShirokamiRyzen">Shirokami Ryzen</a></p>
+</footer>
+
+</div>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home', [
+
+        //logo Header dan Profile
+        'logo' => 'https://avatars.githubusercontent.com/u/76462435?v=4',
+
+        //bebas mau diganti apa aja musiknya hehe
+        //hosted on cloud.ryzendesu.com
+        'judul_lagu' => 'Orange',
+        'Pencipta_lagu' => '7!!',
+        'cover_lagu' => 'assets/img/orange.jpg', //bisa diganti link kalo mau
+        'link_musik' => 'https://cloud.ryzendesu.com/api/name/Orange.mp3?path=/media/Orange.mp3',
+                    //cicak mati njir
+
+        //link platform lagunya
+        'spotify' => 'https://open.spotify.com/track/21htkjP5rYjD3CXG3y9wCT',
+        'apple_music' => 'https://music.apple.com/us/album/orange/982659827',
+        'tidal' => 'https://tidal.com/browse/track/144294816',
+        'deezer' => 'https://www.deezer.com/en/track/976030142',
+        'yt_music' => 'https://music.youtube.com/watch?v=O48gok_FLCg&list=RDAMVMO48gok_FLCg',
+        'amazon_music' => 'https://music.amazon.com/albums/B00VDO364Y',
+
+        //bebas mau lu ganti apa hehe
+        'website' => 'https://profile.ryzendesu.com',
+        'instagram' => 'https://www.instagram.com/ryzen_vermillion',
+        'youtube' => 'https://www.youtube.com/c/AkiraID',
+        'facebook' => 'https://www.facebook.com/Nao.Tomori.UwU',
+        'twitter' => '#',
+        'tiktok' => '#',
+        'threads' => '#',
+        'github' => 'https://github.com/ShirokamiRyzen',
+
+    ]);
+});
